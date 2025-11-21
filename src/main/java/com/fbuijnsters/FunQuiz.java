64c50fcb9ce58d4e01cb3e8e3c6b903f515c349e
Floris Buijnsters <florisbuijnsters@gmail.com>
2025-11-21 01:18:12 +0100

    update FunQuiz.java removed tip

--- src/main/java/com/fbuijnsters/FunQuiz.java
+++ src/main/java/com/fbuijnsters/FunQuiz.java
@@ -1,7 +1,5 @@
 package com.fbuijnsters;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class FunQuiz {
 
     public static void main(String[] args) {
